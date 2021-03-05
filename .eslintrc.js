5be6f7c172da6ab7583ecae3d341cf15452cbceb
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-03-05 22:02:18 +0100

    created project boilerplate with eslint, prettier and husky

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:jest/recommended', 'plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['jest', 'react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+  },
+};
