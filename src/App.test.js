37e91e77c560768b4260d8a1bba1383209a0c1a9
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-03-05 20:58:07 +0100

    CRA initial elements cleaned

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header content', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const liElement = screen.getByText(/Time Line/i);
+  expect(liElement).toBeInTheDocument();
 });
