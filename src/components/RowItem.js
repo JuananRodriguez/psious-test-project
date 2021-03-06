cbdd26e443d74060ab6098a0342ee91ea49d449e
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-03-06 13:45:17 +0100

    each item within the row has a separation offset to its left item

--- src/components/RowItem.js
+++ src/components/RowItem.js
@@ -1,25 +1,25 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const colDimension = 10;
+
 const RowItemWrapper = styled.div`
+  margin-left: ${(p) => p.marginOffset * colDimension}px;
+  width: ${(p) => p.widthCols * colDimension}px;
   box-sizing: border-box;
-  width: ${(p) => p.widthCols * 10}px;
   display: inline-block;
   height: 100%;
   background: white;
   border: 1px solid #32a1ce;
 `;
 
-function RowItem({ cols }) {
-  return <RowItemWrapper data-testid="row-item" widthCols={cols} />;
+function RowItem({ cols, offset }) {
+  return <RowItemWrapper data-testid="row-item" widthCols={cols} marginOffset={offset} />;
 }
 
-RowItem.defaultProps = {
-  cols: 10,
-};
-
 RowItem.propTypes = {
-  cols: PropTypes.number,
+  cols: PropTypes.number.isRequired,
+  offset: PropTypes.number.isRequired,
 };
 
 export default RowItem;
