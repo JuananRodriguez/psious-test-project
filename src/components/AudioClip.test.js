ea6f99db68ce63098f70045817fbd344bb9dc396
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-03-14 15:57:07 +0100

    audioClip was removed because deprecated

--- src/components/AudioClip.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import AudioClip from './AudioClip';
-
-test('should render children properly', () => {
-  const childrenText = '::childrenText::';
-  const { getByText } = render(<AudioClip>{childrenText}</AudioClip>);
-  const childElement = getByText(childrenText);
-  expect(childElement).toBeInTheDocument();
-});
